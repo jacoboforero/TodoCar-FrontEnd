2f79554555e08d0e699a16bfc89d4f74a4225ebe
jacoboforero <jacobo.forero@hotmail.com>
2023-07-05 22:37:25 -0400

    Footer changes

--- src/Layout/Footer.svelte
+++ src/Layout/Footer.svelte
@@ -1,65 +1,66 @@
 <script>
-    // You can add your logic here.
-  </script>
-  
-  <style>
-    .footer {
-      width: 100%;
-      height: 350px;
-      background-color: #202324;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      padding: 20px;
-      box-sizing: border-box;
-    }
-    .columns {
-      display: flex;
-      justify-content: space-between;
-      flex: 1;
-    }
-    .column {
+	// You can add your logic here.
+</script>
 
-      flex: 1;
-      margin: 0 10px;
-    }
-    .mainColumns .column h1{
-        color:white;
-        font-size: 18px;
-    }
+<footer class="footer">
+	<div class="columns mainColumns">
+		<div class="column mainColumns">
+			<h1>About</h1>
+		</div>
+		<div class="column mainColumns">
+			<h1>Contact</h1>
+			<a href="tel:+5617061580" id="phoneNumber">561-706-1580</a>
+		</div>
+		<div class="column mainColumns">
+			<h1>Terms of Service & Privacy Policy</h1>
+		</div>
+		<div class="column rightColumn">
+			<h1 id="mainFooterTitle">TodoCar</h1>
+		</div>
+	</div>
+	<div class="bottom">
+		<p>©2023 TodoCar</p>
+		<!-- Social media links goes here -->
+	</div>
+</footer>
 
-#mainFooterTitle{
+<style>
+	.footer {
+		width: 100%;
+		height: 350px;
+		background-color: #202324;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 20px;
+		box-sizing: border-box;
+	}
+	.columns {
+		display: flex;
+		justify-content: space-between;
+		flex: 1;
+	}
+	.column {
+		flex: 1;
+		margin: 0 10px;
+	}
+	.mainColumns .column h1 {
+		color: white;
+		font-size: 18px;
+	}
 
-    font-size: 46px;
-}
+	#mainFooterTitle {
+		font-size: 46px;
+	}
 
+	.bottom {
+		text-align: center;
+	}
+	.bottom p {
+		color: white;
+	}
 
-    .bottom {
-      text-align: center;
-    }
-    .bottom p{
-        color:white;
-    }
-  </style>
-  
-  <footer class="footer">
-    <div class="columns mainColumns">
-      <div class="column mainColumns">
-        <h1>Company</h1>
-      </div>
-      <div class="column mainColumns">
-        <h1>Resources</h1>
-      </div>
-      <div class="column mainColumns">
-        <h1>Product</h1>
-      </div>
-      <div class="column rightColumn">
-        <h1 id="mainFooterTitle">TodoCar</h1>
-      </div>
-    </div>
-    <div class="bottom">
-      <p>©2023 TodoCar</p>
-      <!-- Social media links goes here -->
-    </div>
-  </footer>
-  
+	#phone_number {
+		color: white;
+	}
+</style>
--- src/Layout/Header.svelte
+++ src/Layout/Header.svelte
@@ -1,97 +1,93 @@
+<header>
+	<div class="logo" />
+	<nav>
+		<a href="#">About Us</a>
+		<a href="#">Find Talent</a>
+		<a href="#">Do Work</a>
+		<a href="#">Sign In / Join</a>
+		<a href="#">FAQ</a>
+	</nav>
+	<input class="search-bar" type="search" placeholder="What service are you looking for?" />
+	<select>
+		<option value="en">Lang-Eng</option>
+		<option value="es">Lang-Esp</option>
+		<option value="fr">Lang-Fra</option>
+		<!-- Add more options here for other languages -->
+	</select>
+</header>
+
 <style>
-    
-    header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-sizing: border-box;
-  color: white;
-  
-  padding-right:20px;
-  padding-left:20px;
-  box-sizing: border-box;
-  width: 100%;
-  height: 80px;
-  flex-shrink: 0;
-  background: #212324;
-  position: relative;
-}
-    .logo {
-      width: 52px;
-      height: 37px;
-      background-color: #B91A1A;
-    }
-    nav {
-      display: flex;
-      gap: 40px;
-      margin-right: auto;
-      margin-left: 80px;
-    }
-    nav a {
-      color: white;
-      text-decoration: none;
-      display: flex;
-flex-direction: column;
-flex-shrink: 0;
-color: #FFF;
-font-size: 14px;
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		box-sizing: border-box;
+		color: white;
 
-    }
-    .search-bar {
-        width: 400px;
-height: 25px;
-flex-shrink: 0;
-border-radius: 3px;
-color:#000;
-text-align: left;
-font-family: 'Times New Roman', Times, serif;
-margin-top: 10px;
-font-size: 16px;
+		padding-right: 20px;
+		padding-left: 20px;
+		box-sizing: border-box;
+		width: 100%;
+		height: 80px;
+		flex-shrink: 0;
+		background: #212324;
+		position: relative;
+	}
+	.logo {
+		width: 52px;
+		height: 37px;
+		background-color: #b91a1a;
+		border-radius: 1px;
+	}
+	nav {
+		display: flex;
+		gap: 40px;
+		margin-right: auto;
+		margin-left: 80px;
+	}
+	nav a {
+		color: white;
+		text-decoration: none;
+		display: flex;
+		flex-direction: column;
+		flex-shrink: 0;
+		color: #fff;
+		font-size: 14px;
+	}
+	.search-bar {
+		width: 400px;
+		height: 25px;
+		flex-shrink: 0;
+		border-radius: 3px;
+		color: #000;
+		text-align: left;
+		font-family: 'Times New Roman', Times, serif;
+		margin-top: 10px;
+		font-size: 16px;
 
-margin-right: -20px;
+		margin-right: -20px;
+	}
 
+	select {
+		width: 132px;
+		height: 32px;
+		appearance: none;
+		background: transparent;
+		border: none;
+		color: #fff;
 
-    }
-    
+		font-size: 17px;
+		margin-right: -40px;
 
-    select{
-  width: 132px;
-  height: 32px;
-  appearance: none;
-  background: transparent;
-  border: none;
-  color: #FFF;
+		margin-left: 40px;
+		margin-top: 10px;
+	}
 
-  font-size: 17px;
-  margin-right: -40px;
-  
-  margin-left: 40px;
-  margin-top: 10px;
-}
+	select::-ms-expand {
+		display: none;
+	}
 
-select::-ms-expand {
-  display: none;
-}
-
-select:focus {
-  outline: none;
-}
-  </style>
-  
-  <header>
-    <div class="logo"></div>
-    <nav>
-      <a href="#">About Us</a>
-      <a href="#">Find Talent</a>
-      <a href="#">Do Work</a>
-      <a href="#">Sign In / Join</a>
-    </nav>
-    <input class="search-bar" type="search" placeholder="What service are you looking for?"> 
-    <select>
-      <option value="en">Lang-Eng</option>
-      <option value="es">Lang-Esp</option>
-      <option value="fr">Lang-Fra</option>
-      <!-- Add more options here for other languages -->
-    </select>
-  </header>
-  
+	select:focus {
+		outline: none;
+	}
+</style>
