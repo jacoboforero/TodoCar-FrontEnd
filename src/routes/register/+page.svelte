1beb139ed62da5f04e4474adb78a78ee4e5bdd07
Jacobo Forero <jacobo.forero@hotmail.com>
2024-01-29 19:57:59 -0500

    Added fourth step to register page. Added next button for all the steps in the register page.

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,64 +1,53 @@
 <script>
 	// @ts-nocheck
-	let currentStep = 3;
-	const totalSteps = 6; // Adjust based on the number of steps in your form
+	let currentStep = 1;
+	let userType = 0; // 0 = Both, 1 = Buyer, 2 = Seller
+	const totalSteps = 6;
 
-	let user_options = [
+	let userOptions = [
 		{ id: 1, name: 'Buyer', selected: false },
 		{ id: 2, name: 'Seller', selected: false },
 		{ id: 3, name: 'Both', selected: false }
 	];
 
-	let continue_options = [
+	let continueOptions = [
 		{ id: 1, name: 'Explore', selected: false },
 		{ id: 2, name: 'Finish', selected: false }
 	];
 
-	function toggleSelection(option) {
-		option.selected = !option.selected;
-	}
+	const toggleSelection = (option) => (option.selected = !option.selected);
 
-	function goToNextStep() {
+	const goToNextStep = () => {
 		if (currentStep < totalSteps) {
 			currentStep += 1;
 		}
-	}
+	};
 
-	function handleProfilePictureUpload(event) {
+	const isNextDisabled = () => currentStep === totalSteps;
+
+	const handleProfilePictureUpload = (event) => {
 		const file = event.target.files[0];
-		// You can handle the file upload process here.
-		// For example, you might want to set it to a local state,
-		// display a preview, or directly upload it to your server.
-	}
-
-	// Additional functions for form handling...
+		// Handle file upload logic here.
+	};
 </script>
 
 <body>
 	<div id="main-registration-box">
 		<form id="registration-form">
-			{#if currentStep == 1}
+			{#if currentStep === 1}
 				<div class="username-group">
-					<h3 class="input-text">Your Username</h3>
-					<p><input placeholder="" /></p>
+					<h3>Your Username</h3>
+					<input placeholder="" />
 				</div>
 
 				<div class="option-boxes">
-					<h3 class="option-text">
-						What kind of user will you be? if you are not sure, select both (Don't worry, you can
-						change this later)
-					</h3>
-					{#each user_options as option}
+					<h3>What kind of user will you be? If unsure, select both (changeable later).</h3>
+					{#each userOptions as option}
 						<!-- svelte-ignore a11y-click-events-have-key-events -->
 						<div class="option-box" on:click={() => toggleSelection(option)}>
-							<input
-								class="option-input"
-								type="checkbox"
-								bind:checked={option.selected}
-								id={'option-' + option.id}
-							/>
+							<input type="checkbox" bind:checked={option.selected} id={`option-${option.id}`} />
 							<label
-								for={'option-' + option.id}
+								for={`option-${option.id}`}
 								class="custom-checkbox {option.selected ? 'selected' : ''}"
 							>
 								{option.name}
@@ -67,25 +56,21 @@
 					{/each}
 				</div>
 			{/if}
-			{#if currentStep == 2}
-				<div class="continue-options">
-					<h3 class="input-text">
-						Welcome to TodoCar! You may now explore our services if you wish or finish creating your
-						profile.
-					</h3>
 
+			{#if currentStep === 2}
+				<div class="continue-options">
+					<h3>Welcome to TodoCar! Explore our services or finish your profile.</h3>
 					<div class="continue-options-boxes">
-						{#each continue_options as option}
+						{#each continueOptions as option}
 							<!-- svelte-ignore a11y-click-events-have-key-events -->
 							<div class="option-box" on:click={() => toggleSelection(option)}>
 								<input
-									class="option-input"
 									type="checkbox"
 									bind:checked={option.selected}
-									id={'option-' + option.id}
+									id={`continue-${option.id}`}
 								/>
 								<label
-									for={'option-' + option.id}
+									for={`continue-${option.id}`}
 									class="custom-checkbox {option.selected ? 'selected' : ''}"
 								>
 									{option.name}
@@ -95,40 +80,50 @@
 					</div>
 				</div>
 			{/if}
-			{#if currentStep == 3}
+
+			{#if currentStep === 3}
 				<div class="profile-upload">
-					<h3 class="input-text">Choose a profile picture</h3>
+					<h3>Choose a profile picture</h3>
 					<label for="profile-picture-upload" class="profile-picture-upload-label">
 						<input
 							type="file"
 							id="profile-picture-upload"
 							accept="image/*"
-							style="display: none;"
 							on:change={handleProfilePictureUpload}
 						/>
-						<div class="upload-placeholder">
-							<!-- Placeholder or icon for upload -->
-							Click to upload
-						</div>
+						<div class="upload-placeholder">Click to upload</div>
 					</label>
 					<div class="name-inputs">
-						<h3 class="input-text">
-							Enter your legal full name. (You may be asked to present ID later.)
-						</h3>
+						<h3>Enter your legal full name. (ID verification may be required.)</h3>
 						<input type="text" placeholder="First" class="first-name-input" />
 						<input type="text" placeholder="Last" class="last-name-input" />
 					</div>
 				</div>
 			{/if}
 
-			{#if currentStep == 4}
+			{#if currentStep === 4 && (userType === 1 || userType === 0)}
+				<div class="car-amount">
+					<h3>
+						Enter the make, model, and mileage of your primary vehicle. (More can be added later.)
+					</h3>
+					<label for="vehicle-info-make"> Make</label>
+					<input type="text" id="vehicle-info-make" />
+					<label for="vehicle-info-model">Model (include year)</label>
+					<input type="text" id="vehicle-info-model" />
+					<label for="vehicle-info-mileage">Mileage</label>
+					<input type="text" id="vehicle-info-mileage" />
+				</div>
+			{/if}
+
+			{#if currentStep === 5}
 				<div />
 			{/if}
-			{#if currentStep == 5}
+			{#if currentStep === 6}
 				<div />
 			{/if}
-			{#if currentStep == 6}
-				<div />
+
+			{#if currentStep < totalSteps}
+				<button on:click={goToNextStep} disabled={isNextDisabled()} id="next-button">Next</button>
 			{/if}
 		</form>
 	</div>
@@ -136,87 +131,87 @@
 
 <style>
 	body {
-		margin: 0;
-		padding: 0;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		min-height: 100vh;
 		font-family: 'Jost', sans-serif;
 		background: linear-gradient(to bottom, #b91a1a, #016473);
-	}
-
-	#main-registration-box {
-		width: 350px;
-		height: 500px;
-		background: #fff; /* Changed for a cleaner look */
-		overflow: hidden;
-		border-radius: 10px;
-		width: 40%;
-		height: 80%;
-		padding: 20px;
+		margin: 0;
+		padding: 0;
 	}
 
 	#registration-form {
-		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	#main-registration-box {
+		width: 40%;
+		min-height: 80%; /* Use min-height to allow expansion if needed */
+		background: #fff;
+		border-radius: 10px;
+		padding: 20px;
+		overflow: auto; /* Add overflow handling */
+		display: flex; /* Make it a flex container */
+		flex-direction: column; /* Stack children vertically */
+	}
+	#registration-form {
 		width: 100%;
 		height: 100%;
 	}
 
-	.username-group {
-		text-align: center; /* Center the username group content */
-		margin-top: 70px;
-		margin-bottom: 30px; /* Add more space between the username section and options */
+	.username-group,
+	.profile-upload {
+		text-align: center;
+		margin-top: 30px;
 	}
-
-	.username-group input {
-		height: 50px;
-		width: 40%; /* Adjust the input width as needed */
-		padding: 5px; /* Add some padding for better appearance */
-		margin-top: 10px; /* Space between label and input */
-	}
-
-	.input-text {
-		margin-bottom: 5px; /* Optional: Adjust the space between the title and the input */
-	}
-
 	.continue-options {
-		text-align: center; /* Center the continue options */
-		margin-top: 60px;
-		margin-bottom: 30px; /* Add more space between the continue options and the options */
-		margin-left: 20px;
-		margin-right: 20px;
+		text-align: center;
+		margin-top: 50px;
 	}
 
+	.username-group input,
+	.name-inputs input[type='text'] {
+		width: 80%;
+		height: 40px;
+		margin-top: 10px;
+		padding: 5px;
+	}
+
+	.option-boxes,
 	.continue-options-boxes {
-		display: flex; /* Use flexbox to arrange children horizontally */
-		justify-content: center; /* Center the items horizontally */
-		flex-wrap: wrap; /* Wrap items to the next line if needed */
-
-		margin-top: 40px;
-	}
-
-	.option-boxes {
-		display: flex; /* Use flexbox to arrange children horizontally */
-		justify-content: center; /* Center the items horizontally */
-		flex-wrap: wrap; /* Wrap items to the next line if needed */
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		margin-top: 20px;
 	}
 
 	.option-box {
-		display: inline-flex; /* Use flexbox within each option */
-		align-items: center; /* Vertically align items in the center */
-		margin: 5px; /* Space between boxes */
+		display: inline-flex;
+		align-items: center;
+		margin: 5px;
 		cursor: pointer;
+	}
+
+	#option-1,
+	#option-2,
+	#option-3,
+	#continue-1,
+	#continue-2,
+	#continue-3 {
+		display: none;
 	}
 
 	.custom-checkbox {
 		width: 100px;
 		height: 100px;
 		border: 1px solid #555;
-		display: flex; /* Use flexbox for internal alignment */
-		justify-content: center; /* Center content horizontally */
-		align-items: center; /* Center content vertically */
-		text-align: center; /* Align text in the center */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
 		margin-right: 8px;
 		transition: background-color 0.3s;
 		border-radius: 20px;
@@ -224,57 +219,54 @@
 	}
 
 	.custom-checkbox.selected {
-		background-color: #b91a1a; /* Change color when selected */
+		background-color: #b91a1a;
 	}
 
 	.option-input {
 		display: none; /* Hide the default checkbox */
 	}
 
-	.option-text {
-		text-align: center;
-	}
-
-	.profile-upload {
-		text-align: center;
-		margin-top: 30px; /* Adjust as needed */
-	}
-
-	.profile-picture-upload-label {
-		display: block; /* Change to block if you want the label to take the full width of its container */
-		cursor: pointer;
-		padding: 10px;
-		background: transparent; /* No background since the placeholder has the grey fill */
-		width: fit-content; /* Fit the content size */
-		margin: 0 auto; /* Center the label */
-	}
-
 	.upload-placeholder {
-		width: 230px; /* Width of the circle */
-		height: 230px; /* Height of the circle, same as width for a perfect circle */
-		background: #c0c0c0; /* Grey fill */
-		border-radius: 50%; /* Makes it a circle */
-		margin: 0 auto; /* Center the placeholder horizontally */
-		display: flex; /* Use flexbox for positioning */
-		justify-content: center; /* Center horizontally in the flex container */
-		align-items: center; /* Center vertically in the flex container */
-		text-align: center; /* Center text for single and multi-line support */
-		/* Additional styling if needed */
-	}
-	.name-inputs h3 {
-		margin-top: 20px; /* Space between upload and name title /
-margin-bottom: 10px; / Space between name title and inputs */
-	}
-
-	.name-inputs input[type='text'] {
-		margin: 10px;
-	}
-
-	.first-name-input {
-		/* Additional styles if needed */
+		width: 230px;
+		height: 230px;
+		background: #c0c0c0;
+		border-radius: 50%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 0 auto;
 	}
 
 	.last-name-input {
-		/* Additional styles if needed */
+		margin-bottom: 0;
+	}
+	.car-amount {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.car-amount label,
+	.car-amount input[type='text'] {
+		align-self: center;
+		width: 80%;
+		margin-top: 10px;
+	}
+
+	.car-amount label {
+		align-self: flex-start;
+		margin-left: 10%;
+		font-size: 20px;
+	}
+
+	#next-button {
+		width: 150px;
+		height: 50px;
+		background-color: #095870;
+		color: #fff;
+		border: none;
+		border-radius: 5px;
+		margin-top: 20px;
+		cursor: pointer;
+		margin-top: 50px;
 	}
 </style>
