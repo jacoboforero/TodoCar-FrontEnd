3d49176904885d1aae48c21727298d341f5b6eb7
Jacobo Forero <jacobo.forero@hotmail.com>
2024-02-05 17:41:58 -0500

    added new step for seller-type user in registration form

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	// @ts-nocheck
-	let currentStep = 1;
-	let userType = 0; // 0 = Both, 1 = Buyer, 2 = Seller
+	let currentStep = 4;
+	let userType = 2; // 0 = Both, 1 = Buyer, 2 = Seller
 	const totalSteps = 6;
+
+	//types of possible users
 
 	let userOptions = [
 		{ id: 1, name: 'Buyer', selected: false },
@@ -10,9 +12,25 @@
 		{ id: 3, name: 'Both', selected: false }
 	];
 
+	// user can pick between exploring the site or finishing their profile
 	let continueOptions = [
 		{ id: 1, name: 'Explore', selected: false },
 		{ id: 2, name: 'Finish', selected: false }
+	];
+
+	// array of possible services that the user can select in the form (services they will most likely be interested in)
+	// POPULATED WITH FAKE DATA, will change to real data once backend is connected
+
+	let interestedInServiceOptions = [
+		{ id: 1, name: 'Car Wash', selected: false },
+		{ id: 2, name: 'Car Detailing', selected: false },
+		{ id: 3, name: 'Car Repair', selected: false },
+		{ id: 4, name: 'Car Rental', selected: false },
+		{ id: 5, name: 'Car Sales', selected: false },
+		{ id: 6, name: 'Car Insurance', selected: false },
+		{ id: 7, name: 'Car Financing', selected: false },
+		{ id: 8, name: 'Car Leasing', selected: false },
+		{ id: 9, name: 'Car Shipping', selected: false }
 	];
 
 	const toggleSelection = (option) => (option.selected = !option.selected);
@@ -100,7 +118,9 @@
 					</div>
 				</div>
 			{/if}
+			<!-- From here on, the form is different depending on what type of user the person is-->
 
+			<!-- Step 4 for buyer -->
 			{#if currentStep === 4 && (userType === 1 || userType === 0)}
 				<div class="car-amount">
 					<h3>
@@ -115,8 +135,38 @@
 				</div>
 			{/if}
 
-			{#if currentStep === 5}
-				<div />
+			{#if currentStep === 4 && (userType === 2 || userType === 0)}
+				<div class="seller-primary-job">
+					<h3>
+						What is your primary job or occupation?(This will not limit the amount of services you
+						can offer).
+					</h3>
+					<input type="text" />
+
+					<h3 class="seller-primary-job">
+						What languages do you speak? (enter them comma separated).
+					</h3>
+					<input type="text" />
+				</div>
+			{/if}
+
+			{#if currentStep === 5 && (userType === 1 || userType === 0)}
+				<div class="interestedIn-services">
+					<h3>Please select at least 3 services that you may be interested in:</h3>
+
+					{#each interestedInServiceOptions as option}
+						<!-- svelte-ignore a11y-click-events-have-key-events -->
+						<div class="option-box" on:click={() => toggleSelection(option)}>
+							<input type="checkbox" bind:checked={option.selected} id={`option-${option.id}`} />
+							<label
+								for={`option-${option.id}`}
+								class="services-custom-checkbox {option.selected ? 'selected' : ''}"
+							>
+								{option.name}
+							</label>
+						</div>
+					{/each}
+				</div>
 			{/if}
 			{#if currentStep === 6}
 				<div />
@@ -156,6 +206,7 @@
 		overflow: auto; /* Add overflow handling */
 		display: flex; /* Make it a flex container */
 		flex-direction: column; /* Stack children vertically */
+		align-items: center;
 	}
 	#registration-form {
 		width: 100%;
@@ -205,6 +256,7 @@
 	}
 
 	.custom-checkbox {
+		box-sizing: border-box;
 		width: 100px;
 		height: 100px;
 		border: 1px solid #555;
@@ -222,8 +274,16 @@
 		background-color: #b91a1a;
 	}
 
-	.option-input {
-		display: none; /* Hide the default checkbox */
+	#option-1,
+	#option-2,
+	#option-3,
+	#option-4,
+	#option-5,
+	#option-6,
+	#option-7,
+	#option-8,
+	#option-9 {
+		display: none;
 	}
 
 	.upload-placeholder {
@@ -258,6 +318,20 @@
 		font-size: 20px;
 	}
 
+	.seller-primary-job {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		margin-top: 30px;
+		margin-left: 30px;
+		margin-right: 30px;
+	}
+	.seller-primary-job input[type='text'] {
+		margin-top: 10px;
+		padding: 5px;
+		margin-bottom: 30px;
+	}
+
 	#next-button {
 		width: 150px;
 		height: 50px;
@@ -268,5 +342,36 @@
 		margin-top: 20px;
 		cursor: pointer;
 		margin-top: 50px;
+	}
+
+	.interestedIn-services {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	.services-custom-checkbox {
+		box-sizing: border-box;
+		width: 150px; /* Adjusted for 3 per row */
+		height: 90px; /* Adjusted for a more square appearance */
+		border: 1px solid #555;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		margin: 8px; /* Added margin for spacing */
+		transition: background-color 0.3s;
+		border-radius: 20px;
+		background-color: #fefefe;
+	}
+
+	.services-custom-checkbox.selected {
+		background-color: #b91a1a;
+	}
+
+	.interestedIn-services h3 {
+		margin-bottom: 40px;
 	}
 </style>
